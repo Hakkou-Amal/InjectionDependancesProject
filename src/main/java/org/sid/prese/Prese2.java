18008257331724858595165e075ae1e625266de3
Your Name <you@example.com>
2025-04-03 23:25:24 +0000

    nvlle mise a j

--- src/main/java/org/sid/prese/Prese2.java
+++ src/main/java/org/sid/prese/Prese2.java
@@ -1,8 +1,17 @@
 package org.sid.prese;
+import org.sid.dao.IDao;
+
+import java .util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Prese2 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Scanner scanner = new Scanner(new File( "config.txt"));
+        String daoClassName = scanner.nextLine();
+        Class cDao = Class.forName(daoClassName);
+         IDao  dao =(IDao) cDao.newInstance();
+         System.out.println(dao.getData());
 
     }
 }
